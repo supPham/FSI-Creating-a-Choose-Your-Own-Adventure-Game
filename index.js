8f1dd5e8bf28270978e06671ad62281e44f1bd19
supPham <panpham@outlook.com>
2022-10-19 00:41:58 -0400

    first answer path

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Your Code here
+let firstAnswer  = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+
+} else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
+
+
+
